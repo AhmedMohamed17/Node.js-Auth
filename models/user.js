42e25cec14f721ac94d53069da717d64b7077e19
ahmedmohamed17 <ahmedmohamed7sayed@gmail.com>
2024-03-13 02:18:49 +0200

    added validation and Hashing passwords :eyes:

--- models/user.js
+++ models/user.js
@@ -1,17 +1,26 @@
 const mongoose = require("mongoose");
-
+const { isEmail } = require("validator");
+const bcrypt = require("bcrypt");
 const userSchmea = new mongoose.Schema({
   email: {
     type: String,
-    required: true,
+    required: [true, "please Enter an Email"],
     unique: true,
     lowercase: true,
+    validate: [isEmail, "please enter a valid email"],
   },
   password: {
     type: String,
-    required: true,
-    minLength: 6,
+    required: [true, "please Enter an password"],
+    minLength: [6, "min password length is 6 char"],
   },
+});
+// fire function before doc saved to db
+userSchmea.pre("save", async function (next) {
+  const salt = await bcrypt.genSalt();
+  this.password = await bcrypt.hash(this.password, salt);
+  //   console.log("this ya beez", this);
+  next();
 });
 
 const User = mongoose.model("user", userSchmea);
