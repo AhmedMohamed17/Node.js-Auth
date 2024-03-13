105b01ced226c80a00db9d993371bee9d2ca1893
ahmedmohamed17 <ahmedmohamed7sayed@gmail.com>
2024-03-13 11:07:41 +0200

    added cookies

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const authRoutes = require("./routes/authRoutes");
+const cookieParser = require("cookie-parser");
 const app = express();
 const port = 3000;
 // middleware
-
+app.use(cookieParser());
 app.use(express.static("public"));
 app.use(express.json());
 // view engine
@@ -23,3 +24,20 @@ mongoose
 app.get("/", (req, res) => res.render("home"));
 app.get("/smoothies", (req, res) => res.render("smoothies"));
 app.use(authRoutes);
+
+app.get("/set-cookies", (req, res) => {
+  // res.setHeader("Set-cookie", "newUser=true");
+  res.cookie("newUser", false);
+  res.cookie("isEmployee", true, {
+    maxAge: 1000 * 60 * 60 * 24,
+    httpOnly: true,
+  });
+
+  res.send("you got the cookies");
+});
+
+app.get("/read-cookie", (req, res) => {
+  const cookies = req.cookies;
+  console.log(cookies);
+  res.json(cookies);
+});
